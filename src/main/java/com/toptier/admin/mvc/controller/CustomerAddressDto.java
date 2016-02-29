3f927fa7b41b37fca672e48b317cc083bd339565
Cristal Wu <youyouzhixin@gmail.com>
2016-02-29 19:46:36 +1100

    Customer Edit Page

---
+++ src/main/java/com/toptier/admin/mvc/controller/CustomerAddressDto.java
@@ -0,0 +1,67 @@
+package com.toptier.admin.mvc.controller;
+
+public class CustomerAddressDto {
+	private Integer customerId;
+	private String customerName;
+	private String email;
+	private String phone;
+	private String addrlines;
+	private String region;
+	private String postcode;
+	
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddrlines() {
+		return addrlines;
+	}
+	
+	public void setAddrlines(String addrlines){
+		this.addrlines = addrlines;
+	}
+	
+	public String getRegion() {
+		return region;
+	}
+	
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	
+	public String getPostcode() {
+		return postcode;
+	}
+	
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+}
